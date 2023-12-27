cde59bc226f64f58478e8bc34a00473ce0d6c76c
Sagittarius87 <Jarik-87@yandex.ru>
2023-12-27 21:00:08 +0300

    Переименовал папку images в sprites. Сделал главное меню и скрыл канвас. В index.html div блоки меню заменил на button. В game.js обьявил метод setEvants() и вызвал его в init(). Добавил обработку событий при нажатии на button.

--- scripts/game.js
+++ scripts/game.js
@@ -9,9 +9,25 @@ let game = {
     sounds: {
         heroStep: null
     },
+    gameUI: {
+        buttonNewGame: null,
+        buttonContinue: null
+    },
     init() {
         this.ctx = document.getElementById("canvas").getContext("2d")
+        this.gameUI.buttonNewGame = document.getElementById("newgame")
+        this.gameUI.buttonContinue = document.getElementById("continue")
+
+        this.setEvants()
+    },
+    setEvants() {
+        this.gameUI.buttonNewGame.onclick = () => {
+            console.log("buttonNewGame click")
+        }
+        this.gameUI.buttonContinue.onclick = () => {
+            console.log("buttonContinue click")
+        }
     }
 }
 
-console.log(game.init)
+game.init()
