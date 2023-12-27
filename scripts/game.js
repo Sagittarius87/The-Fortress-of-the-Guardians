dbf724d923da01c6c942ed2ac1ad71544b411d38
Sagittarius87 <Jarik-87@yandex.ru>
2023-12-27 17:24:09 +0300

    В index.html добавил div блоки для меню и выровнял их. В game.js добавил объект game и свойства ctx, gameRunning, sprites, sounds для него. Сделал реализацию метода init()

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,17 @@
-console.log('Hello World!')
+let game = {
+    ctx: null,
+    gameRunning: false,
+    sprites: {
+        hero: null,
+        tile: null,
+        wall: null
+    },
+    sounds: {
+        heroStep: null
+    },
+    init() {
+        this.ctx = document.getElementById("canvas").getContext("2d")
+    }
+}
+
+console.log(game.init)
